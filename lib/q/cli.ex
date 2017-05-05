128720ca951d7b045ca4245d5fdeb0f8619a5ad3
Samuel Mullen <samuel@samuelmullen.com>
2017-05-05 12:41:49 -0500

    Use config to determine where to read/write

--- lib/q/cli.ex
+++ lib/q/cli.ex
@@ -1,9 +1,10 @@
 defmodule Q.CLI do
-  @qpath Application.get_env(:q, :path)
+  @qrc_path Path.expand(Application.get_env(:q, :qrc_path))
 
-  def run(argv) do
+  def main(argv) do
     argv
     |> parse_args
+    |> parse_config
     |> process
   end
 
@@ -12,17 +13,19 @@ defmodule Q.CLI do
                                        aliases:  [h: :help])
 
     case options do
-      {[help: true], _, _} 
-        -> :help
-
-      {_, [], _} 
-        -> :list
-
-      {_, argv, _} 
-        -> {:add, argv}
-
-      _ -> :help
+      {[help: true], _, _} -> :help
+      {_, [], _}           -> :list
+      {_, item, _}         -> {:add, item}
+      _                    -> :help
     end
+  end
+
+  def parse_config(:help), do: :help
+  def parse_config(:list) do 
+    {:list, do_parse_config(@qrc_path, File.exists?(@qrc_path))}
+  end
+  def parse_config({:add, item}) do
+    {:add, item, do_parse_config(@qrc_path, File.exists?(@qrc_path))}
   end
 
   def process(:help) do
@@ -31,14 +34,30 @@ defmodule Q.CLI do
     """
   end
 
-  def process(:list) do
-    case File.read(@qpath) do
+  def process({:list, filepath}) do
+    case File.read(filepath) do
       {:ok, data} -> IO.write data
       {:error, error} -> IO.puts "Error: #{:file.format_error(error)}"
     end
   end
 
-  def process({:add, argv}) do
-    File.write(@qpath, Enum.join(argv, " ") <> "\n", [:append])
+  def process({:add, argv, filepath}) do
+    File.write(filepath, Enum.join(argv, " ") <> "\n", [:append])
+  end
+
+  defp do_parse_config(path, true) do
+    {:ok, results} = ConfigParser.parse_file(path)
+    results["default"]["qrc_path"]
+  end
+  defp do_parse_config(path, false) do
+    content = "[default]\nqrc_path = ~/queue.txt\n"
+
+    case File.write(path, content) do
+      :ok -> 
+        do_parse_config(path, true)
+
+      {:error, message} -> 
+        IO.puts(:stderr, message) && System.halt(2)
+    end
   end
 end
