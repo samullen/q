3afcf188e1fe432c9d202e410518dcaeea888fc0
Samuel Mullen <samuel@samuelmullen.com>
2017-05-05 12:41:34 -0500

    Add tests for processing config

--- test/cli_test.exs
+++ test/cli_test.exs
@@ -2,18 +2,50 @@ defmodule CliTest do
   use ExUnit.Case
   doctest Q
 
-  import Q.CLI, only: [parse_args: 1]
+  import Q.CLI, only: [parse_args: 1, parse_config: 1]
 
-  test ":help returned by option parsing with -h and --help options" do
-    assert parse_args(["-h", "anything"]) == :help
-    assert parse_args(["--help", "anything"]) == :help
+  describe "parse_args/1" do
+    test ":help returned by option parsing with -h and --help options" do
+      assert parse_args(["-h", "anything"]) == :help
+      assert parse_args(["--help", "anything"]) == :help
+    end
+
+    test ":list returned if no arguments provided" do
+      assert parse_args([]) == :list
+    end
+
+    test ":add returned if arguments provided" do
+      assert parse_args(~w{this is a test}) == {:add, ~w{this is a test}}
+    end
   end
 
-  test ":list returned if no arguments provided" do
-    assert parse_args([]) == :list
-  end
+  describe "parse_config/1" do
+    test ":help is returned when :help is passed" do
+      assert parse_config(:help) == :help
+    end
 
-  test ":add returned if arguments provided" do
-    assert parse_args(~w{this is a test}) == {:add, ~w{this is a test}}
+    test "passing list returns {:list, path}" do
+      assert parse_config(:list) == {:list, "test/fixtures/files/queue.txt"}
+    end
+
+    test "it creates a .qrc file in the configured qrc path if nonexistent" do
+      qrc_path = Path.expand(Application.get_env(:q, :qrc_path))
+      File.rename(qrc_path, "#{Path.dirname(qrc_path)}/.qrc_bak")
+
+      parse_config(:list)
+      assert File.exists?(qrc_path) == true
+
+      File.rename("#{Path.dirname(qrc_path)}/.qrc_bak", qrc_path)
+    end
+
+    test "it sets defaults for created .qrc" do
+      qrc_path = Path.expand(Application.get_env(:q, :qrc_path))
+      File.rename(qrc_path, "#{Path.dirname(qrc_path)}/.qrc_bak")
+
+      parse_config(:list)
+      assert File.read(qrc_path) == {:ok, "[default]\nqrc_path = ~/queue.txt\n"}
+
+      File.rename("#{Path.dirname(qrc_path)}/.qrc_bak", qrc_path)
+    end
   end
 end
